9033ee7bac26d00ff3e414a58eeb1317d9196598
Nivesh Jain <niveshj@Niveshs-Mac-mini.local>
2026-04-21 14:20:36 +0530

    updated quartz config

--- quartz.config.ts
+++ quartz.config.ts
@@ -86,7 +86,7 @@ const config: QuartzConfig = {
         enableSiteMap: true,
         enableRSS: true,
       }),
-      // Plugin.Assets(),
+      Plugin.Assets(),
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
